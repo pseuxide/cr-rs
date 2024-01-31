7cc8d7f8a88ba3367e03f165ff4244dbbe77404e
pseuxide <s3pt3mb3r@protonmail.com>
2024-01-31 22:36:37 +0900

    port fixed

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,7 +1,7 @@
 use actix_cors::Cors;
 use actix_web::http::header;
 use actix_web::middleware::Logger;
-use actix_web::{web, App, HttpServer};
+use actix_web::{ web, App, HttpServer };
 use crud_rust::api;
 use crud_rust::infrastructures::database::postgres::pg_pool;
 use std::env;
@@ -10,7 +10,13 @@ use std::env;
 async fn main() -> std::io::Result<()> {
     logging_setup();
 
-    let allowed_origin = env::var("ALLOWED_ORIGIN").unwrap_or_else(|_| "http://localhost:3000".to_string());
+    // Retrieve the port number from the environment variable
+    let port = env::var("PORT").unwrap_or_else(|_| "8080".to_string());
+    let address = format!("0.0.0.0:{}", port);
+
+    let allowed_origin = env
+        ::var("ALLOWED_ORIGIN")
+        .unwrap_or_else(|_| "http://localhost:3000".to_string());
 
     let pool = pg_pool();
 
@@ -20,18 +26,18 @@ async fn main() -> std::io::Result<()> {
             .wrap(Logger::default())
             .wrap(
                 Cors::default()
-                    .allowed_origin(allowed_origin.as_str())
+                    .allowed_origin(&allowed_origin)
                     .allowed_methods(vec!["GET", "POST", "PUT", "DELETE"])
                     .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
                     .allowed_header(header::CONTENT_TYPE)
                     .max_age(3600)
-                    .supports_credentials(), // Allow the cookie auth.
+                    .supports_credentials() // Allow the cookie auth.
             )
             .service(web::scope("/api").configure(api::post::api_config))
     })
-    .bind("0.0.0.0:8080")?
-    .run()
-    .await
+        .bind(&address)
+        ? // Bind to the dynamically assigned port
+        .run().await
 }
 
 fn logging_setup() {
