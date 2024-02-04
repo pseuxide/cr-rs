9aa4c3ee656a822c97acd82cd45addb165350bd7
pseuxide <s3pt3mb3r@protonmail.com>
2024-02-04 21:53:49 +0900

    new import block

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,13 +14,19 @@ provider "google" {
   region      = "asia-northeast1"
 }
 
+import {
+  id = "crud-rust-instance"
+  to = google_sql_database_instance.postgres
+}
+
+# postgres sql
 resource "google_sql_database_instance" "postgres" {
   name             = "crud-rust-instance"
   region           = "asia-northeast1"
   database_version = "POSTGRES_15"
 
   settings {
-    tier = "db-f1-micro" # Very small machine type with the lowest cost
+    tier = "db-f1-micro"
 
     backup_configuration {
       enabled = false
@@ -32,17 +38,24 @@ resource "google_sql_database_instance" "postgres" {
   }
 }
 
+# creating database inside cloud sql instance
 resource "google_sql_database" "crud_rust_database" {
   name     = "crud-rust-database"
   instance = google_sql_database_instance.postgres.name
 }
 
+# creating user in cloud sql
 resource "google_sql_user" "default" {
   name     = "postgres"
   instance = google_sql_database_instance.postgres.name
   password = var.db_password
 }
 
+import {
+  id = var.gar_repo_name
+  to = google_artifact_registry_repository.gar_repo
+}
+# creating artifact registry
 resource "google_artifact_registry_repository" "gar_repo" {
   provider = google
 
@@ -61,6 +74,5 @@ output "instance_connection_name" {
 }
 
 output "database_url" {
-  # public ipじゃなくていいのか？
   value = "postgres://postgres:${var.db_password}@/${google_sql_database_instance.postgres.connection_name}:5432/${google_sql_database.crud_rust_database.name}?host=/cloudsql/${google_sql_database_instance.postgres.connection_name}&sslmode=disable"
 }
